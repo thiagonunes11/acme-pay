519d69d642ebd1bfccb330e94e87dfdbf68534fc
Diego <diegopaiva04@gmail.com>
2024-05-11 10:54:16 -0300

    Init transaction

--- acme-pay-transaction-service/src/main/java/br/com/acmepay/application/domain/models/TransactionDomain.java
+++ acme-pay-transaction-service/src/main/java/br/com/acmepay/application/domain/models/TransactionDomain.java
@@ -1,4 +1,19 @@
 package br.com.acmepay.application.domain.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TransactionDomain {
+    private Long id;
+    private LocalDateTime data_transaction;
+    private Integer source_account;
+    private Integer destination_account;
+    private BigDecimal ammout;
 }
